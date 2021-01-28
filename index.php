1b891fd38de8c320de8fab7eb8ae45f9d2b16cca
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-01-28 01:33:17 +0100

    Completed2

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    <h1>Hello world</h1>
-    ?>
-</body>
+    <title>Badwords</title>
+  </head>
+  <body>
+        <?php
+          $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+          $badword = $_GET["badword"];
+          $newText = str_replace($badword, "***", $text)
+        ?>
+
+        <p>
+        <?php 
+            echo strlen($newPar); 
+        ?>
+        </p>
+  </body>
 </html>
